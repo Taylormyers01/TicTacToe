b1b6eeb58d9a47385d5c9f110187abc5f5e4098d
taylor <taylormyers01@comcast.net>
2023-07-07 10:20:46 -0400

    updated all functionality within board

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,68 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] playingBoard = new Character[3][3];
     public Board(Character[][] matrix) {
+        this.playingBoard = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return (checkColumn('X') || checkRows('X')|| checkDiag('X'));
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return (checkColumn('O') || checkRows('O')|| checkDiag('O'));
     }
 
     public Boolean isTie() {
-        return null;
+        if(!isInFavorOfO() && !isInFavorOfX() ){
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        if(isInFavorOfX()){
+            return "X";
+        }
+        else if(isInFavorOfO()){
+            return "O";
+        }
+        return "";
+    }
+    public boolean checkRows(char player){
+        for(int i = 0; i < 3; i++){
+            if(playingBoard[i][0] == player && playingBoard[i][1] == player && playingBoard[i][2] == player){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean checkColumn(char player){
+        for(int i = 0; i < 3; i++){
+            if(playingBoard[0][i] == player && playingBoard[1][i] == player && playingBoard[2][i] == player){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean checkDiag(char player){
+        if(playingBoard[0][0] == player && playingBoard[1][1] == player && playingBoard[2][2] == player){
+            return true;
+        }
+        else if(playingBoard[0][2] == player && playingBoard[1][1] == player && playingBoard[2][0] == player){
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Character[][] hold =  {{'X', 'X', 'O'},
+                               {'O', 'X', 'X'},
+                               {'X', 'O', 'O'}};
+        Board player = new Board(hold);
+        System.out.println(player.isInFavorOfX());
     }
 
 }
